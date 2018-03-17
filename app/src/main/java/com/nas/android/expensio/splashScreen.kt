2baa7d657a7cdb05104544bd57c34cd6fcc6c748
Surya <surya.thiru001@gmail.com>
2018-03-17 14:35:34 +0530

    minor change to test ignore

--- app/src/main/java/com/nas/android/expensio/splashScreen.kt
+++ app/src/main/java/com/nas/android/expensio/splashScreen.kt
@@ -5,7 +5,7 @@ import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class splashScreen : AppCompatActivity() {
+class SplashScreen : AppCompatActivity() {
 
     private var mDelayHandler: Handler? = null
     private val SPLASH_DELAY: Long = 3000 //3 seconds
