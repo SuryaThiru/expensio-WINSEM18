8977978f3147bf74ba5a9ad45166163394eea312
Surya <surya.thiru001@gmail.com>
2018-03-17 16:09:09 +0530

    realm db configs'

--- app/src/main/java/com/nas/android/expensio/HomeActivity.kt
+++ app/src/main/java/com/nas/android/expensio/HomeActivity.kt
@@ -3,10 +3,21 @@ package com.nas.android.expensio
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        // test database
+        Realm.init(this)
+        var realmconfig = RealmConfiguration.Builder()
+                .name("test.realm")
+                .build()
+        Realm.setDefaultConfiguration(realmconfig)
     }
 }
