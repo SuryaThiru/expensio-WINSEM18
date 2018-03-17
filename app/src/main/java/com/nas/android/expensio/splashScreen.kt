9e350f63ed4adf93821de1376f64373e64efe965
N-eh-A <methegreat4@gmail.com>
2018-03-17 05:47:49 +0530

    Add splash screen and redirect to home

--- app/src/main/java/com/nas/android/expensio/splashScreen.kt
+++ app/src/main/java/com/nas/android/expensio/splashScreen.kt
@@ -1,12 +1,44 @@
 package com.nas.android.expensio
 
+import android.content.Intent
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class splashScreen : AppCompatActivity() {
 
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        //Initialize the Handler
+
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
     }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
+
 }
