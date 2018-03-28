8c358e0e974bf342ecceefd47b01a5cef6912b1e
N-eh-A <methegreat4@gmail.com>
2018-03-28 12:30:55 +0530

    few changes

--- app/src/main/java/com/nas/android/expensio/AddDebtOrLoan.kt
+++ app/src/main/java/com/nas/android/expensio/AddDebtOrLoan.kt
@@ -3,6 +3,7 @@ package com.nas.android.expensio
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import kotlinx.android.synthetic.main.activity_add_debt_or_loan.*
 import kotlinx.android.synthetic.main.app_bar_main_navigation.*
 
 class AddDebtOrLoan : AppCompatActivity() {
@@ -11,9 +12,11 @@ class AddDebtOrLoan : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_debt_or_loan)
 
-        fab.setOnClickListener { view ->
+        fab_add_debt_loan.setOnClickListener { view ->
             Snackbar.make(view, "Detail Added", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
+
     }
 }
